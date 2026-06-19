32870feb6d29a11a749361856d67df568060f1eb
Natarizki <natakeren890@gmail.com>
2026-06-19 12:57:11 +0700

    Disable shellcheck SC2034 for metadata vars

--- make.sh
+++ make.sh
@@ -5,7 +5,9 @@ BPKG_NAME="bpkg"
 BPKG_VERSION="0.1.0"
 BPKG_DEPENDS="bash curl git jq"
 BPKG_SHA256SUM="7b92f0b36ebf41c505f57782f17a7792c852251557d5d963bb5c310c403c6a24"
+# shellcheck disable=SC2034
 BPKG_TARBALL_SHA256="2ba750d56a6929a9651aa31872687f5eadc9e9ffdc587857e571f45a8bea8607"
+# shellcheck disable=SC2034
 BPKG_TARBALL_URL="https://github.com/bada-lang-org/bpkg/archive/refs/tags/v${BPKG_VERSION}.tar.gz"
 
 echo "Building ${BPKG_NAME} v${BPKG_VERSION}"
